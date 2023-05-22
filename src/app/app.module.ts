607ce3a0cd8cd2b0136c0489567f42b0ab6483ba
DESKTOP-G7EI8GJ\mrivera <martin.rivera@collectdemexico.com>
2023-05-22 17:02:09 -0600

    creacion de segundo componente desde angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
-  declarations: [AppComponent, Test1Component],
+  declarations: [AppComponent, Test1Component, Test2Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
